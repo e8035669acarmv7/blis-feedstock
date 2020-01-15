0fb660e0c7bf83e1dcd7f004983176206c2d3359
Isuru Fernando <isuruf@gmail.com>
2020-01-15 16:43:48 +0530

    Let conda_build_wrapper set env variables

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,15 +1,15 @@
 CFLAGS=$(echo "${CFLAGS}" | sed "s/-march=[a-zA-Z0-9]*//g")
 CFLAGS=$(echo "${CFLAGS}" | sed "s/-mtune=[a-zA-Z0-9]*//g")
 
-case `uname` in
-    Darwin)
+case $target_platform in
+    osx-*)
         export CC=$BUILD_PREFIX/bin/clang
         ./configure --prefix=$PREFIX --enable-cblas --enable-threading=pthreads intel64
         make CC_VENDOR=clang -j${CPU_COUNT}
         make install
         make check -j${CPU_COUNT}
         ;;
-    Linux)
+    linux-*)
         ln -s `which $CC` $BUILD_PREFIX/bin/gcc
         export CC=$BUILD_PREFIX/bin/gcc
         ./configure --prefix=$PREFIX --enable-cblas --enable-threading=pthreads x86_64
@@ -17,18 +17,8 @@ case `uname` in
         make install
         make check -j${CPU_COUNT}
         ;;
-    MINGW*)
-        export PATH="$PREFIX/Library/bin:$BUILD_PREFIX/Library/bin:$PATH"
-        export CC=clang
-        export RANLIB=echo
+    win-*)
         export LIBPTHREAD=
-        export AS=llvm-as
-        export AR=llvm-ar
-        export CFLAGS="-MD -I$PREFIX/Library/include"
-        export LDFLAGS="$LDFLAGS -L$PREFIX/Library/lib"
-        clang --version
-        llvm-as --version
-        llvm-ar --version
         ./configure --disable-shared --enable-static --prefix=$PREFIX/Library --enable-cblas --enable-threading=pthreads --enable-arg-max-hack x86_64
         make -j${CPU_COUNT}
         make install
