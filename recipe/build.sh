6df682ae4045f2394dcbf35e882554bbfaa89746
Yu-Ren Zhang <e8035669@gmail.com>
2024-07-11 23:24:57 +0800

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,5 @@
-CFLAGS=$(echo "${CFLAGS}" | sed "s/-march=[a-zA-Z0-9]*//g")
-CFLAGS=$(echo "${CFLAGS}" | sed "s/-mtune=[a-zA-Z0-9]*//g")
+CFLAGS=$(echo "${CFLAGS}" | sed "s/-march=[a-zA-Z0-9-]*//g")
+CFLAGS=$(echo "${CFLAGS}" | sed "s/-mtune=[a-zA-Z0-9-]*//g")
 
 # Avoid sorting LDFLAGS
 sed -i.bak 's/LDFLAGS := $(sort $(LDFLAGS))//g' common.mk
